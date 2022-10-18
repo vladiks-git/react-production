4b7bcf9f31c7f6c0d5293043b3e01dd001462a36
AXBIT\vershov <vershov@clink.tech>
2022-10-18 12:49:28 +0400

    lazy loading components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import React from "react";
-import { Counter } from "./components/Counter";
+import React, { Suspense } from "react";
+import { Link } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import "./index.scss";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 
-export const App = () => {
+const App = () => {
   return (
     <div className="app">
-      gregerg
-      <Counter />
+      <Link to={"/"}>Main</Link>
+      <Link to={"/about"}>About</Link>
+      <Suspense fallback={<div>Loading..</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
+
+export default App;
---
+++ src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AboutPage = () => {
+  return <div>AboujjjjjjjjjkmuykuykyujjjtPage</div>;
+};
+
+export default AboutPage;
