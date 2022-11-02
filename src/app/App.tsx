f78f393999049b6e96251be0746b3b087d549dc1
vershov <vershov@clink.tech>
2022-11-02 22:02:14 +0400

    add routing config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,11 @@
 import React, { Suspense } from 'react';
 import { Link } from 'react-router-dom';
-import { Route, Routes } from 'react-router-dom';
 import { classNames } from 'shared/lib/ClassNames/classNames';
 
 import { useTheme } from 'app/providers/ThemeProvider';
 
 import './styles/index.scss';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
+import { AppRouter } from './providers/router';
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -18,12 +16,7 @@ const App = () => {
       <Link to={'/'}>Main</Link>
       <Link to={'/about'}>About</Link>
       <button onClick={toggleTheme}> change theme</button>
-      <Suspense fallback={<div>Loading..</div>}>
-        <Routes>
-          <Route path={'/about'} element={<AboutPage />} />
-          <Route path={'/'} element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
---
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -0,0 +1,19 @@
+import React, { Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+
+const AppRouter = () => {
+  return (
+    <div>
+      <Suspense fallback={<div>Loading..</div>}>
+        <Routes>
+          {Object.values(routeConfig).map(({ path, element }) => (
+            <Route key={path} path={path} element={element} />
+          ))}
+        </Routes>
+      </Suspense>
+    </div>
+  );
+};
+
+export default AppRouter;
