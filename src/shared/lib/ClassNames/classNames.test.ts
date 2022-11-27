9aad670e0f6e46087892b11fcce7db21846f6298
vershov <vershov@clink.tech>
2022-11-27 21:35:24 +0400

    add some tests

--- src/shared/lib/ClassNames/classNames.test.ts
+++ src/shared/lib/ClassNames/classNames.test.ts
@@ -1,5 +1,12 @@
+import { classNames } from './classNames';
 describe('classNames', () => {
-  test('test', () => {
-    expect(true).toBe(true);
+  test('with one argument', () => {
+    expect(classNames('class1')).toBe('class1');
+  });
+
+  test('with additional classes', () => {
+    expect(classNames('someClass', {}, ['class1 class2'])).toBe(
+      'someClass class1 class2'
+    );
   });
 });
